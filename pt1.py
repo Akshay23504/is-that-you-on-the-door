28444363d9d579cca24b39f452eb6ba6e41ff8f8
Akshay23504 <aaravind@syr.edu>
2018-10-01 00:12:02 -0400

    Completed step 1 of pre-processing. 1000 more to goGetting data for a time range now.

---
+++ pt1.py
@@ -0,0 +1,54 @@
+import csv
+import datetime
+
+path = "../Dataset/Sample/Sample_6/50/"
+filename = "HandPhoneGyrUser50S1.csv"
+filename_checkpoint = "HandPhoneCheckpointsUser50S1.csv"
+door_entry_checkpoints = []
+door_exit_checkpoints = []
+stair_entry_checkpoints = []
+stair_exit_checkpoints = []
+center_points = []
+entries = {}
+window_size = 2  # In seconds
+
+
+def convert_string_to_time(time_in_str):
+    return datetime.datetime.strptime(time_in_str, '%Y-%m-%d %H:%M:%S.%f')
+
+
+def calculate_center_point(entry_time, exit_time):
+    return datetime.datetime.fromtimestamp((entry_time.timestamp() + exit_time.timestamp()) / 2)
+
+
+with open(path + filename_checkpoint) as input_file:
+    next(input_file)  # Skip the first line
+    read_input = csv.reader(input_file, delimiter=',')
+    for row in read_input:
+        checkpoint_time = convert_string_to_time(row[2])
+        if row[1] == "DoorEntry":
+            door_entry_checkpoints.append(checkpoint_time)
+        elif row[1] == "DoorExit":
+            door_exit_checkpoints.append(checkpoint_time)
+        elif row[1] == "StairEntry":
+            stair_entry_checkpoints.append(checkpoint_time)
+        elif row[1] == "StairExit":
+            stair_exit_checkpoints.append(checkpoint_time)
+
+for i in range(len(door_entry_checkpoints)):
+    center_points.append(calculate_center_point(door_entry_checkpoints[i], door_exit_checkpoints[i]))
+
+
+with open(path + filename) as input_file:
+    next(input_file)  # Skip the first line
+    read_input = csv.reader(input_file, delimiter=',')
+    for row in read_input:
+        time = convert_string_to_time(row[4])
+        for i in range(len(center_points)):
+            if (center_points[i] - datetime.timedelta(seconds=window_size)) <= time <= \
+                    (center_points[i] + datetime.timedelta(seconds=window_size)):
+                if entries.get(i + 1) is None:
+                    entries[i + 1] = []
+                entries[i + 1].append((row[1], row[2], row[3], row[4]))
+
+print("")
