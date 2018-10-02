2917bf1b5a3c70dea95b90777d02c494ebaa4dfb
Akshay23504 <aaravind@syr.edu>
2018-10-01 20:26:52 -0400

    Code to put the results into csv

--- pt1.py
+++ pt1.py
@@ -1,16 +1,16 @@
 import csv
 import datetime
 
-path = "../Dataset/Sample/Sample_6/50/"
-filename = "HandPhoneGyrUser50S1.csv"
-filename_checkpoint = "HandPhoneCheckpointsUser50S1.csv"
+path = "../Dataset/Sample/Sample_6/53/"
+filename = "HandPhoneGyrUser53S1.csv"
+filename_checkpoint = "HandPhoneCheckpointsUser53S1.csv"
 door_entry_checkpoints = []
 door_exit_checkpoints = []
 stair_entry_checkpoints = []
 stair_exit_checkpoints = []
 center_points = []
 entries = {}
-window_size = 2  # In seconds
+window_size = 6  # In seconds
 
 
 def convert_string_to_time(time_in_str):
@@ -49,6 +49,12 @@ with open(path + filename) as input_file:
                     (center_points[i] + datetime.timedelta(seconds=window_size)):
                 if entries.get(i + 1) is None:
                     entries[i + 1] = []
-                entries[i + 1].append((row[1], row[2], row[3], row[4]))
+                entries[i + 1].append([row[1], row[2], row[3], row[4]])
 
-print("")
+# Code to put the results into CSV
+# counter = 1
+# for d in entries:
+#     with open("../Dataset/Sample/Sample_6_small/53/HandPhoneGyrUser53S1_door" + str(counter) + ".csv", "w+") as o_file:
+#         csv.writer(o_file).writerows(entries.get(counter))
+#         counter += 1
+
